76de13d90ab96019257e136f11c7023d0c3bd970
z9487xd <135353453+z9487xd@users.noreply.github.com>
2024-03-16 22:15:55 +0800

    Update cards.py

--- 偵探遊戲/cards.py
+++ 偵探遊戲/cards.py
@@ -1,8 +1,17 @@
 from os import path
 import pygame
 
-
-class Cards:
+cards = []
+class Player:
+    def __init__(self, name: str):
+        self.name = name
+        self.hand_card = []
+        self.hand_card.append(self)
+    def remove():
+        pass
+    def add():
+        pass
+class Card:
     def __init__(self, size: float, name: str, x: float=1 , y: float= 1, owner: str="物品") -> None:
         self.width = size 
         self.height = size
@@ -10,15 +19,13 @@ class Cards:
         self.x = x
         self.y = y
         self.name = name
-
+        cards.append(self)
         self.imageOriginal = pygame.Surface((self.width,self.height))
         self.imageOriginal.fill((255,255,255))
         self.imageOriginal.blit(source = pygame.transform.scale(pygame.image.load(path.join("image/"+name+".png")).convert_alpha(),(180, 180)), dest = (0,0))
         self.imageOriginal.set_colorkey((255,255,255))
         self.image = self.imageOriginal.copy()
     
-    def ability(self):
-        ...
     
     def use(self):
         ...
@@ -34,4 +41,11 @@ class Cards:
 
     def draw(self, surface: pygame.surface.Surface):
         surface.blit(self.image, (self.x,self.y))
+
+class Take(Card):
+    def __init__(self, size: float, x: float=1, y: float=1, owner: str="物品") -> None:
+        super().__init__(size, "拿取", x, y, owner)
+    
+    def ability(self):
+        if self.owner == "":
 
