a19ee14fc463bf7d5ffe0722c0ae8131a362677f
robin <robin.liu.9669.game@gmail.com>
2024-03-28 10:24:55 +0800

    room selet section is done robin

--- 偵探遊戲/main.py
+++ 偵探遊戲/main.py
@@ -9,9 +9,11 @@ class Screen:
     def __init__(self, width: int=None, height: int=None) -> None:
         self.width, self.height = width, height
         self.width, self.height = self.info()
+        print(self.width, self.height)
         if self.width/self.height != 1.6:
             self.resize()
         self.screen = pygame.display.set_mode((self.width, self.height))
+        print(self.width, self.height)
         pygame.display.set_caption("Detective Game")
     
     def resize(self):
@@ -45,19 +47,25 @@ def init():
 def room_selection(screen: Screen, rooms: list[r.Room], room_map: r.Map) -> r.Room:
     nearst: r.Room = None
     running = True
+    print(screen.width*2/5, screen.height/16*11.5)
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
             if event.type == pygame.MOUSEBUTTONDOWN:
+                print("touch")
                 if nearst != None:
                     running = False
         screen.fill()
         
         mouse_x, mouse_y = pygame.mouse.get_pos()
-        nearst = room_map.detect(mouse_x, mouse_y, screen.info())
-        
+        # print(mouse_x, mouse_y)
+        nearst = room_map.detect(mouse_x, mouse_y, screen.info(), rooms, nearst)
+        print(nearst)
         room_map.update(screen.screen)
+        
+        for room in rooms:
+            room.drawC(screen.screen, screen.width)
         
         screen.flip()
     return nearst
@@ -83,7 +91,8 @@ def main() -> int:
     Online, rooms, screen = init()
     
     # roomlist.append(Room("kitchen"), Room("bedroom"), Room("yard"),Room("study"), Room("liviingroom"))
-    rooms.append(r.Room("kitchen", screen.info(), screen.width/1.5, screen.height/1.5))
+    rooms.append(r.Room("kitchen", screen.info(), screen.width*2/5, screen.height/16*11.5))
+    
     room_map = r.Map(screen.info(), (screen.width, screen.height), rooms)
     for room in rooms:
         room.data_update(Online.datas[room.name])
@@ -101,7 +110,8 @@ def main() -> int:
     the_room = room_selection(screen, rooms, room_map)
     
     print("entering the room...")
-    enter_room(Online, screen, the_room)
+    if the_room != None:
+        enter_room(Online, screen, the_room)
 
 
 
--- 偵探遊戲/room.py
+++ 偵探遊戲/room.py
@@ -10,11 +10,18 @@ class Room:
         self.players = []
         self.width = size[0]
         self.height = size[1]
+        self.hightlight = False
         self.imageOriginal = pygame.Surface((self.width, self.height))
         self.imageOriginal.fill((255,255,255))
-        self.imageOriginal.blit(source = pygame.transform.scale(pygame.image.load(path.join("偵探遊戲/image/"+room_name+".png")).convert_alpha(),(180, 180)), dest = (0,0))
+        self.imageOriginal.blit(source = pygame.transform.scale(pygame.image.load(path.join("偵探遊戲/image/"+room_name+".png")).convert_alpha(), size), dest = (0,0))
         self.imageOriginal.set_colorkey((255,255,255))
         self.image = self.imageOriginal.copy()
+        
+    def drawC(self, surface: pygame.surface.Surface, height: int) -> None:
+        if not self.hightlight:
+            pygame.draw.circle(surface, (100, 100, 10), (self.x, self.y), height/100)
+        else:
+            pygame.draw.circle(surface, (150, 150, 10), (self.x, self.y), height/50)
     
     def update(self, surface: pygame.surface.Surface, data: client.Datas) -> None:
         self.draw(surface)
@@ -38,22 +45,22 @@ class Room:
         
     def draw(self, surface: pygame.surface.Surface) -> int:
         try:
-            surface.blit(self.image, (self.x,self.y))
+            surface.blit(self.image, (0, 0))
             return 0
         except Exception as e:
-            print(e)
+            print(f"room draw:{e}")
             return 1
         
 class Map:
     def __init__(self, screen_info: tuple[int, int]=(0, 0), size: tuple[int, int]=(0, 0), rooms: Room=None) -> None:
-        self.x = screen_info[0]/2-size[0]/2
-        self.y = screen_info[1]/2-size[1]/2
+        self.x = screen_info[0]/2-size[0]*2/5
+        self.y = screen_info[1]/2-size[1]*2/5
         self.width = size[0]
         self.height = size[1]
-        self.room_list: list[Room] = rooms
+        
         self.imageOriginal = pygame.Surface((self.width, self.height))
         self.imageOriginal.fill((255, 255, 255))
-        self.imageOriginal.blit(source = pygame.transform.scale(pygame.image.load(path.join("偵探遊戲/image/地圖.png")).convert_alpha(),(180, 180)), dest = (0,0))
+        self.imageOriginal.blit(source = pygame.transform.scale(pygame.image.load(path.join("偵探遊戲/image/地圖.png")).convert_alpha(),(size[0]*4/5, size[1]*4/5)), dest = (0,0))
         self.imageOriginal.set_colorkey((255, 255, 255))
         self.image = self.imageOriginal.copy()
 
@@ -65,11 +72,21 @@ class Map:
     def update(self, surface: pygame.surface.Surface) -> None:
         self.draw(surface)
     
-    def detect(self, x: int, y: int, screen_info: tuple[int, int]) -> Room:
-        nearst = None
-        for room in self.room_list:
-            if nearst == None and self.distance(x, y, room) < screen_info[0]/100: nearst = room
-            elif nearst != None and self.distance(x, y, room) < self.distance(x, y, nearst)  and self.distance(x, y, room) < screen_info[0]/100: nearst = room
+    def detect(self, x: int, y: int, screen_info: tuple[int, int], rooms: list[Room], nearst: Room=None) -> Room:
+        for room in rooms:
+            if nearst == None and self.distance(x, y, room) < screen_info[0]/50:
+                nearst = room
+                nearst.hightlight = True
+            elif nearst != None and self.distance(x, y, room) < self.distance(x, y, nearst)  and self.distance(x, y, room) < screen_info[0]/50:
+                nearst.hightlight = False
+                nearst = room
+                nearst.hightlight = True
+                
+        if nearst != None and self.distance(x, y, nearst) > screen_info[0]/50:
+            print("too far away")
+            nearst.hightlight = False
+            nearst = None
+        
         return nearst
         
     def draw(self, surface: pygame.surface.Surface) -> int:
